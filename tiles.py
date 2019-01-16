6e180859581017c52e47d3b3e0b4a98d83ab1200
som1sezhi <s89jssy22@gmail.com>
2019-01-16 11:46:08 -0500

    code cleanup in tiles.py

--- tiles.py
+++ tiles.py
@@ -1,23 +1,21 @@
 import pygame, os
 pygame.init()
 
-class Tile:
-    def __init__(self, sprite, outline_mode=0):
-        self.sprite = sprite
-        self.outline_mode = outline_mode
+def load_sprite(name):
+    return pygame.image.load(os.path.join('data', name + '.png')).convert_alpha()
 
-outline_corner = pygame.image.load(os.path.join('data', 'outline_corner.png')).convert_alpha()
-outline_1side = pygame.image.load(os.path.join('data', 'outline_1side.png')).convert_alpha()
-outline_2sides = pygame.image.load(os.path.join('data', 'outline_2sides.png')).convert_alpha()
-outline_3sides = pygame.image.load(os.path.join('data', 'outline_3sides.png')).convert_alpha()
-outline_4sides = pygame.image.load(os.path.join('data', 'outline_4sides.png')).convert_alpha()
-outline_normal = [outline_corner, outline_1side, outline_2sides, outline_3sides, outline_4sides]
-outline_corner_factory = pygame.image.load(os.path.join('data', 'outline_corner_factory.png')).convert_alpha()
-outline_1side_factory = pygame.image.load(os.path.join('data', 'outline_1side_factory.png')).convert_alpha()
-outline_2sides_factory = pygame.image.load(os.path.join('data', 'outline_2sides_factory.png')).convert_alpha()
-outline_3sides_factory = pygame.image.load(os.path.join('data', 'outline_3sides_factory.png')).convert_alpha()
-outline_4sides_factory = pygame.image.load(os.path.join('data', 'outline_4sides_factory.png')).convert_alpha()
-outline_factory = [outline_corner_factory, outline_1side_factory, outline_2sides_factory, outline_3sides_factory, outline_4sides_factory]
+outline_normal = [load_sprite('outline_corner'),
+                  load_sprite('outline_1side'),
+                  load_sprite('outline_2sides'),
+                  load_sprite('outline_3sides'),
+                  load_sprite('outline_4sides')]
+outline_factory = [load_sprite('outline_corner_factory'),
+                  load_sprite('outline_1side_factory'),
+                  load_sprite('outline_2sides_factory'),
+                  load_sprite('outline_3sides_factory'),
+                  load_sprite('outline_4sides_factory')]
+
+
 
 def get_outlines(sides, corners, outline_graphics):
     o_corner, o_1side, o_2sides, o_3sides, o_4sides = outline_graphics
@@ -60,16 +58,20 @@ def get_outlines(sides, corners, outline_graphics):
                 outlines.blit(pygame.transform.rotate(o_corner, -90 * i + 180), (0, 0))
     return outlines
 
-def t_path(name):
-    return os.path.join('data', name + '.png')
+
+class Tile:
+    def __init__(self, sprite, outline_mode=0):
+        self.sprite = sprite
+        self.outline_mode = outline_mode
+        
 
 tiles = {}
-tiles['/'] = Tile(pygame.image.load(t_path('wall_red')).convert(), 1)
-tiles['8'] = Tile(pygame.image.load(t_path('wall_green')).convert(), 1)
-tiles['w'] = Tile(pygame.image.load(t_path('wall_purple')).convert(), 1)
-tiles['€'] = Tile(pygame.image.load(t_path('wall_tan')).convert(), 1)
-tiles['²'] = Tile(pygame.image.load(t_path('wall_factorygrey')).convert(), 2)
-tiles['¼'] = Tile(pygame.image.load(t_path('wall_factorydarkgrey')).convert(), 2)
-tiles['¶'] = Tile(pygame.image.load(t_path('wall_factoryyellow')).convert(), 2)
-tiles['º'] = Tile(pygame.image.load(t_path('wall_factoryred')).convert(), 2)
-tiles['/B'] = Tile(pygame.image.load(t_path('wall_yellow')).convert(), 1)
+tiles['/'] = Tile(load_sprite('wall_red'), outline_mode=1)
+tiles['8'] = Tile(load_sprite('wall_green'), outline_mode=1)
+tiles['w'] = Tile(load_sprite('wall_purple'), outline_mode=1)
+tiles['€'] = Tile(load_sprite('wall_tan'), outline_mode=1)
+tiles['²'] = Tile(load_sprite('wall_factorygrey'), outline_mode=2)
+tiles['¼'] = Tile(load_sprite('wall_factorydarkgrey'), outline_mode=2)
+tiles['¶'] = Tile(load_sprite('wall_factoryyellow'), outline_mode=2)
+tiles['º'] = Tile(load_sprite('wall_factoryred'), outline_mode=2)
+tiles['/B'] = Tile(load_sprite('wall_yellow'), outline_mode=1)
